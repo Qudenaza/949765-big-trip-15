a75e5ee1227f2d9d72c12d288f11bb826963508d
Qudenaza <qudenaza@gmail.com>
2021-08-07 10:51:16 +0300

    Революция или эволюция? (часть 2)

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import TripPointView from './view/trip-point.js';
 import { generateTripPoint } from './mock/trip.js';
 import { render, RenderPosition } from './utils.js';
 
-const TRIP_POINTS_COUNT = 4;
+const TRIP_POINTS_COUNT = 16;
 
 const tripPoints = new Array(TRIP_POINTS_COUNT).fill().map(() => generateTripPoint()).sort((a, b) => new Date(a.dateFrom) - new Date(b.dateFrom));
 
@@ -32,14 +32,34 @@ const renderTripPoint = (tripPointsListElement, tripPoint) => {
     tripPointsListElement.replaceChild(tripPointComponent.getElement(),  tripPointEditComponent.getElement());
   };
 
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+
+      replaceFormToPoint();
+
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
   tripPointComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', () => {
     replacePointToForm();
+
+    document.addEventListener('keydown', onEscKeyDown);
   });
 
   tripPointEditComponent.getElement().querySelector('form').addEventListener('submit', (evt) => {
     evt.preventDefault();
 
     replaceFormToPoint();
+
+    document.removeEventListener('keydown', onEscKeyDown);
+  });
+
+  tripPointEditComponent.getElement().querySelector('form .event__rollup-btn').addEventListener('click', () => {
+    replaceFormToPoint();
+
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
   render(tripPointsListElement, tripPointComponent.getElement(), RenderPosition.BEFOREEND);
